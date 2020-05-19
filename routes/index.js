8c7a64e96f77849dac61e3cfc56072439f950b13
Giovanni Kaaijk <giovanni@goedgemerkt.nl>
2020-05-19 12:36:11 +0200

    added api route for client side

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const api = require('../modules/api');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/data', function(req, res, next) {
+  api().then(function(result) {res.json({data: result})})
+})
 
 module.exports = router;

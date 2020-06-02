29e0c8f20a0da7d4b814750cbc632022cfaf8a09
lennartdeknikker <me@lenn4rt.com>
2020-06-02 09:46:18 +0200

    update routes

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-var dayGoalRouter = require('./routes/daygoal')
+const indexRouter = require('./routes/index')
+const dayGoalRouter = require('./routes/daygoal')
 
-var app = express()
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -21,7 +20,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 app.use('/dagdoel', dayGoalRouter)
 
 // catch 404 and forward to error handler
@@ -30,7 +28,7 @@ app.use(function(req, res, next) {
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message
     res.locals.error = req.app.get('env') === 'development' ? err : {}
--- public/scripts/daygoal.js
+++ public/scripts/daygoal.js
@@ -1,27 +1,27 @@
 // set the dimensions and margins of the graph
 const margin = {top: 20, right: 20, bottom: 150, left: 40},
     width = 960 - margin.left - margin.right,
-    height = 650 - margin.top - margin.bottom;
+    height = 650 - margin.top - margin.bottom
 
 // set the ranges
 const x = d3.scaleBand()
-          .range([0, width])
-          .padding(0.1);
+    .range([0, width])
+    .padding(0.1)
 const y = d3.scaleLinear()
-          .range([height, 0]);
+    .range([height, 0])
           
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-const svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+const svg = d3.select('body').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 
+        'translate(' + margin.left + ',' + margin.top + ')')
 // get url + fetch to get data
-const getUrl = window.location;
-const baseUrl = getUrl .protocol + "//" + getUrl.host
+const getUrl = window.location
+const baseUrl = getUrl .protocol + '//' + getUrl.host
 fetch(baseUrl + '/pamdata/PA043F3')
     .then(res => res.json())
     .then(res => {
@@ -42,45 +42,45 @@ fetch(baseUrl + '/pamdata/PA043F3')
         }
 
         // Scale the range of the data in the domains
-        x.domain(data.firstWeek.map((d) => { return d.date; }));
-        y.domain([0, d3.max(data.firstWeek, (d) => { return d.totaalpunten; })]);
+        x.domain(data.firstWeek.map((d) => { return d.date }))
+        y.domain([0, d3.max(data.firstWeek, (d) => { return d.totaalpunten })])
 
         // append the rectangles for the bar chart
-        svg.selectAll(".bar")
-        .data(data.firstWeek)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.date); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.totaalpunten); })
-        .attr("height", function(d) { return height - y(d.totaalpunten); })
-        .style("fill", (d) => getColor(d.totaalpunten))
+        svg.selectAll('.bar')
+            .data(data.firstWeek)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', function(d) { return x(d.date) })
+            .attr('width', x.bandwidth())
+            .attr('y', function(d) { return y(d.totaalpunten) })
+            .attr('height', function(d) { return height - y(d.totaalpunten) })
+            .style('fill', (d) => getColor(d.totaalpunten))
 
         // add the x Axis
-        svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll('text')
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-65)");
+        svg.append('g')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(d3.axisBottom(x))
+            .selectAll('text')
+            .style('text-anchor', 'end')
+            .attr('dx', '-.8em')
+            .attr('dy', '.15em')
+            .attr('transform', 'rotate(-65)')
 
         // add the y Axis
-        svg.append("g")
-        .call(d3.axisLeft(y));
+        svg.append('g')
+            .call(d3.axisLeft(y))
 
-         // add the goal line
-         svg.append("line")
-         .attr("class", "goal")
-         .attr('x1', 0)
-         .attr('x2', width)
-         .attr('y1', y(data.goalweek1))
-         .attr('y2', y(data.goalweek1))
-         .attr('stroke-width', 1)
-         .attr('stroke-dasharray', '10,10')
-         .attr('stroke', 'black')
-        })
+        // add the goal line
+        svg.append('line')
+            .attr('class', 'goal')
+            .attr('x1', 0)
+            .attr('x2', width)
+            .attr('y1', y(data.goalweek1))
+            .attr('y2', y(data.goalweek1))
+            .attr('stroke-width', 1)
+            .attr('stroke-dasharray', '10,10')
+            .attr('stroke', 'black')
+    })
 
        
 
